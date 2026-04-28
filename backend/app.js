77f3ba9f8dc2128084f4b200112f3c5f4d98edd4
Carter Moyer <moyer6141@uwlax.edu>
2026-04-27 20:12:48 -0500

    fix: use permissive iframe headers (ALLOWALL, frame-ancestors *) matching delish-healthy-food

--- backend/app.js
+++ backend/app.js
@@ -110,7 +110,10 @@ const connectDB = async () => {
 connectDB();
 
 // --- Middlewares ---
-app.use(helmet());
+app.use(helmet({
+  frameguard: false,
+  contentSecurityPolicy: false
+}));
 
 app.use(cors({
   origin: true,
